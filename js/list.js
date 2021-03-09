3c83d4952b52db447590486b71490da570f1c606
tanglinxue <tanglinxuework@163.com>
2021-03-09 20:56:48 +0800

    tang

---
+++ js/list.js
@@ -0,0 +1,73 @@
+function getLocation() {
+	console.log(navigator.geolocation)
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
+	} else {
+		alert("浏览器不支持地理定位。");
+	}
+}
+
+function showPosition(position) {
+	console.log(position);
+	var lat = position.coords.latitude; //纬度 
+	var lag = position.coords.longitude; //经度 
+	console.log('纬度:' + lat + ',经度:' + lag);
+}
+
+function showError(error) {
+	switch (error.code) {
+		case error.PERMISSION_DENIED:
+			console.log("定位失败,用户拒绝请求地理定位");
+			break;
+		case error.POSITION_UNAVAILABLE:
+			console.log("定位失败,位置信息是不可用");
+			break;
+		case error.TIMEOUT:
+			console.log("定位失败,请求获取用户位置超时");
+			break;
+		case error.UNKNOWN_ERROR:
+			console.log("定位失败,定位系统失效");
+			break;
+	}
+}
+window.onload = function() {
+	var citys = pro_city_data.citylist;
+	var oS1 = document.getElementsByTagName('select');
+	for (var i = 0; i < citys.length; i++) {
+		oS1[0].innerHTML += '<option value="' + citys[i].p + '">' + citys[i].p + '</option>';
+	}
+	oS1[0].onchange = function() {
+		var value = oS1[0].value
+		oS1[1].innerHTML = '<option>城市</option>';
+		for (var i = 0; i < citys.length; i++) {
+			if (value == citys[i].p) {
+				for (var j = 0; j < citys[i].c.length; j++) {
+					oS1[1].innerHTML += '<option value="' + citys[i].c[j].n + '">' + citys[i].c[j].n +
+						'</option>'
+				}
+			}
+
+		}
+	}
+	$('#toTop').click(function() {
+		pageSwiper.slideTo(0,500);
+	})
+	
+	getLocation();
+	var pageSwiper = new Swiper('#pageSwiper', {
+		direction: 'vertical',
+		mousewheel: true,
+		speed: 800,
+		delay: 3000,
+		slidesPerView: 'auto'
+	})
+
+	var bannerSwiper = new Swiper('#bannerSwiper', {
+		speed: 800,
+		delay: 3000,
+		navigation: {
+			nextEl: '.t-index-banner-next',
+			prevEl: '.t-index-banner-prev',
+		},
+	})
+}
