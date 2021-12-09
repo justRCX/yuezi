76c965edab93f6a4ce69d416b15091205bb3e28d
rlj <you@example.com>
2021-12-09 15:55:14 +0800

    update

--- js/header.js
+++ js/header.js
@@ -3,4 +3,11 @@ var offCanvasWrapper = mui('#offCanvasWrapper');
 document.getElementById('offCanvasShow').addEventListener('tap', function() {
     offCanvasWrapper.offCanvas('show');
   });
+//主界面和侧滑菜单界面均支持区域滚动；
   mui('#offCanvasSideScroll').scroll();
+  mui('#offCanvasContentScroll').scroll();
+  // 关闭侧边栏滑动
+  var offCanvasInner = offCanvasWrapper[0].querySelector('.mui-inner-wrap');
+offCanvasInner.addEventListener('drag', function(event) {
+    event.stopPropagation();
+});
