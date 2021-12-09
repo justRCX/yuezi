de2533adf2eab01f70d8aa7f803784e93e230130
rlj <you@example.com>
2021-12-09 20:10:51 +0800

    update

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
-mui.init();
-var offCanvasWrapper = mui('#offCanvasWrapper');
-document.getElementById('offCanvasShow').addEventListener('tap', function() {
-    offCanvasWrapper.offCanvas('show');
-  });
-//主界面和侧滑菜单界面均支持区域滚动；
-  mui('#offCanvasSideScroll').scroll();
-  mui('#offCanvasContentScroll').scroll();
-  // 关闭侧边栏滑动
-  var offCanvasInner = offCanvasWrapper[0].querySelector('.mui-inner-wrap');
-offCanvasInner.addEventListener('drag', function(event) {
-    event.stopPropagation();
-});
+document.getElementById('offCanvasShow').onclick = function(){
+  $('body').addClass('g-fullscreen g-active');
+  $('#aside').addClass('aside-active')
+  document.getElementById('aside').style = "visibility: visible; transform: translate3d(0px, 0px, 0px);"
+}
+$('.canvas-backdrop').on('click',function(){
+    document.getElementById('aside').style = 'visibility: visible; transform: translate3d(-300px, 0px, 0px);'
+    $('body').removeClass('g-fullscreen g-active');
+    setTimeout(function(){
+      $('#aside').removeClass('aside-active')
+      document.getElementById('aside').style = 'visibility: hidden; transform: translate3d(-300px, 0px, 0px);'
+    },200)
+})
